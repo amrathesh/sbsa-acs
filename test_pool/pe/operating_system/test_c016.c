355c1cf040da3ea58cf51302b5cf6a4cf7bc875d
Amrathesh <amrathesh@arm.com>
2024-01-12 16:22:42 +0530

    PE test 16, check for CPOR support for LLC presented as mem-side
     - Added support to parse and check for cpor support by memory
       side last level cache.
     - Level 1 mem-side cache present near to memory is considered
       as mem-side LLC.
    
    Signed-off-by: Amrathesh <amrathesh@arm.com>

--- test_pool/pe/operating_system/test_c016.c
+++ test_pool/pe/operating_system/test_c016.c
@@ -24,17 +24,24 @@
 #define TEST_RULE  "S_MPAM_PE"
 #define TEST_DESC  "Check MPAM LLC Requirements       "
 
+#define MEM_CACHE_LEVEL_1 1
+
 static void payload(void)
 {
     uint32_t index = val_pe_get_index_mpid(val_pe_get_mpid());
     uint32_t msc_node_cnt;
     uint32_t rsrc_node_cnt;
     uint32_t msc_index, rsrc_index;
-    uint32_t llc_index;
-    uint64_t cache_identifier;
+    uint32_t pptt_llc_index;
+    uint64_t pptt_cache_id;
     uint32_t test_run = 0;
     uint32_t ris_supported = 0;
     uint32_t cpor_supported = 0;
+    uint32_t pe_prox_domain;
+    uint32_t pptt_llc_cpor_supported = 0;
+    uint32_t memside_llc_cpor_supported = 0;
+    uint64_t msc_rsrc_desc1;
+    uint64_t msc_rsrc_desc2;
 
     if (g_sbsa_level < 5) {
         val_set_status(index, RESULT_SKIP(g_sbsa_level, TEST_NUM, 01));
@@ -44,22 +51,22 @@ static void payload(void)
     /* Check if PE implements FEAT_MPAM */
     if (!((VAL_EXTRACT_BITS(val_pe_reg_read(ID_AA64PFR0_EL1), 40, 43) > 0) ||
         (VAL_EXTRACT_BITS(val_pe_reg_read(ID_AA64PFR1_EL1), 16, 19) > 0))) {
-            val_set_status(index, RESULT_SKIP(g_sbsa_level, TEST_NUM, 01));
+            val_set_status(index, RESULT_SKIP(g_sbsa_level, TEST_NUM, 02));
             return;
     }
 
     /* Find the LLC cache identifier */
-    llc_index = val_cache_get_llc_index();
-    if (llc_index == CACHE_TABLE_EMPTY) {
+    pptt_llc_index = val_cache_get_llc_index();
+    if (pptt_llc_index == CACHE_TABLE_EMPTY) {
         val_print(AVS_PRINT_ERR, "\n       PPTT table empty", 0);
-        val_set_status(index, RESULT_FAIL(g_sbsa_level, TEST_NUM, 01));
+        val_set_status(index, RESULT_SKIP(g_sbsa_level, TEST_NUM, 03));
         return;
     }
-    cache_identifier = val_cache_get_info(CACHE_ID, llc_index);
+    pptt_cache_id = val_cache_get_info(CACHE_ID, pptt_llc_index);
 
-    if (cache_identifier == INVALID_CACHE_INFO) {
+    if (pptt_cache_id == INVALID_CACHE_INFO) {
         val_print(AVS_PRINT_ERR, "\n       LLC invalid in PPTT", 0);
-        val_set_status(index, RESULT_FAIL(g_sbsa_level, TEST_NUM, 02));
+        val_set_status(index, RESULT_SKIP(g_sbsa_level, TEST_NUM, 04));
         return;
     }
 
@@ -69,7 +76,7 @@ static void payload(void)
 
     if (msc_node_cnt == 0) {
         val_print(AVS_PRINT_ERR, "\n       MSC count is 0", 0);
-        val_set_status(index, RESULT_FAIL(g_sbsa_level, TEST_NUM, 03));
+        val_set_status(index, RESULT_FAIL(g_sbsa_level, TEST_NUM, 01));
         return;
     }
 
@@ -87,16 +94,16 @@ static void payload(void)
 
         for (rsrc_index = 0; rsrc_index < rsrc_node_cnt; rsrc_index++) {
 
-            /* check whether the resource location is cache */
+            /* check whether the resource location is PE cache */
             if (val_mpam_get_info(MPAM_MSC_RSRC_TYPE, msc_index, rsrc_index) ==
-                                                                       MPAM_RSRC_TYPE_PE_CACHE) {
-                if (val_mpam_get_info(MPAM_MSC_RSRC_DESC1, msc_index, rsrc_index) ==
-                                                                               cache_identifier) {
-                    /* We have MSC which controls/monitors the LLC cache */
-                    val_print(AVS_PRINT_DEBUG, "\n       rsrc index  = %d", rsrc_index);
+                MPAM_RSRC_TYPE_PE_CACHE) {
+                val_print(AVS_PRINT_DEBUG, "\n       rsrc index  = %d", rsrc_index);
+                msc_rsrc_desc1 = val_mpam_get_info(MPAM_MSC_RSRC_DESC1, msc_index, rsrc_index);
 
+                /* match pptt llc cache id */
+                val_print(AVS_PRINT_DEBUG, "\n       rsrc descriptor 1  = %llx", msc_rsrc_desc1);
+                if (msc_rsrc_desc1 == pptt_cache_id) {
                     test_run = 1;
-
                     /* Select resource instance if RIS feature implemented */
                     if (ris_supported)
                         val_mpam_memory_configure_ris_sel(msc_index, rsrc_index);
@@ -118,16 +125,76 @@ static void payload(void)
     }
 
     if (!test_run) {
-        val_print(AVS_PRINT_ERR, "\n       No LLC MSC found", 0);
-        val_set_status(index, RESULT_FAIL(g_sbsa_level, TEST_NUM, 05));
-        return;
+        val_print(AVS_PRINT_ERR, "\n       No MSC found on PPTT LLC", 0);
     } else if (!cpor_supported) {
-        val_print(AVS_PRINT_ERR, "\n       CPOR unsupported by LLC", 0);
-        val_set_status(index, RESULT_FAIL(g_sbsa_level, TEST_NUM, 04));
-        return;
+        val_print(AVS_PRINT_ERR, "\n       CPOR unsupported by PPTT LLC", 0);
+    } else {
+        pptt_llc_cpor_supported = 1;
     }
 
-    val_set_status(index, RESULT_PASS(g_sbsa_level, TEST_NUM, 01));
+    /* test mem-side cache for cpor */
+    val_print(AVS_PRINT_DEBUG, "\n\n       Testing mem-side caches for CPOR support", 0);
+    cpor_supported = 0;
+    test_run = 0;
+    pe_prox_domain = val_srat_get_info(SRAT_GICC_PROX_DOMAIN, val_pe_get_uid(index));
+    /* visit each MSC node and check for mem cache resources */
+    for (msc_index = 0; msc_index < msc_node_cnt; msc_index++) {
+        rsrc_node_cnt = val_mpam_get_info(MPAM_MSC_RSRC_COUNT, msc_index, 0);
+
+        val_print(AVS_PRINT_DEBUG, "\n       MSC index  = %d", msc_index);
+        val_print(AVS_PRINT_DEBUG, "\n       Resource count = %d", rsrc_node_cnt);
+
+        ris_supported = val_mpam_msc_supports_ris(msc_index);
+        val_print(AVS_PRINT_INFO, "\n       RIS support = %d", ris_supported);
+
+        for (rsrc_index = 0; rsrc_index < rsrc_node_cnt; rsrc_index++) {
+            /* check whether the resource type is a mem-cache */
+            if (val_mpam_get_info(MPAM_MSC_RSRC_TYPE, msc_index, rsrc_index) ==
+                MPAM_RSRC_TYPE_MEM_SIDE_CACHE) {
+                val_print(AVS_PRINT_DEBUG, "\n       rsrc index  = %d", rsrc_index);
+                msc_rsrc_desc1 = val_mpam_get_info(MPAM_MSC_RSRC_DESC1, msc_index, rsrc_index);
+                msc_rsrc_desc2 = val_mpam_get_info(MPAM_MSC_RSRC_DESC2, msc_index, rsrc_index);
+                val_print(AVS_PRINT_DEBUG, "\n       rsrc descriptor 1  = %llx", msc_rsrc_desc1);
+                val_print(AVS_PRINT_DEBUG, "\n       rsrc descriptor 2  = %llx", msc_rsrc_desc2);
+
+                /* check if mem-side cache matches with PE proximity domain
+                   and cache level == 1 for mem-side LLC (based on assumption that mem-cache nearer to memory is LLC) */
+                if ((msc_rsrc_desc2 == pe_prox_domain) && (msc_rsrc_desc1 == MEM_CACHE_LEVEL_1)) {
+                    test_run = 1;
+                    /* Select resource instance if RIS feature implemented */
+                    if (ris_supported)
+                        val_mpam_memory_configure_ris_sel(msc_index, rsrc_index);
+
+                    /* Check CPOR are present */
+                    if (val_mpam_supports_cpor(msc_index)) {
+                        val_print(AVS_PRINT_DEBUG,
+                                  "\n       CPOR Supported by mem-side cache with rsrc_index %d", rsrc_index);
+                        cpor_supported |= 1;
+                    }
+                    val_print(AVS_PRINT_DEBUG,
+                              "\n       CPOR Not Supported by mem-side cache with rsrc_index %d", rsrc_index);
+                }
+            }
+        }
+    }
+
+    if (!test_run) {
+        val_print(AVS_PRINT_ERR, "\n       No MSC found on mem-side LLC", 0);
+    } else if (!cpor_supported) {
+        val_print(AVS_PRINT_ERR, "\n       CPOR unsupported by mem-side LLC", 0);
+    } else {
+        memside_llc_cpor_supported = 1;
+    }
+
+    /* if either of PPTT LLC or mem-side LLC supports cache portioning (CPOR) pass the test */
+    if (pptt_llc_cpor_supported || memside_llc_cpor_supported) {
+        val_set_status(index, RESULT_PASS(g_sbsa_level, TEST_NUM, 01));
+    }
+    else {
+        val_set_status(index, RESULT_FAIL(g_sbsa_level, TEST_NUM, 02));
+    }
+
+    return;
 }
 
 uint32_t c016_entry(uint32_t num_pe)
